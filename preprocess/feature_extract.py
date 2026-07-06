Fixed overflow error in material_value by switching to uint16

preprocess/feature_extract.py
@@ -45,7 +45,7 @@ class ChessFeatureExtractor:
         self.black_indices = np.full(32, self.INT16_MAX, dtype=np.uint16)
         self.pieces_char = [""] * 32
         self.pieces_sq = np.zeros(32, dtype=np.int32)
-        self.material_value = np.zeros(1, dtype=np.uint8)
+        self.material_value = np.zeros(1, dtype=np.uint16)
         self.turn = np.zeros(1, dtype=np.uint8)
         self.score = np.zeros(1, dtype=np.float32)
 

train/dataset.py
@@ -17,7 +17,7 @@ def parse_tfrecord_fn(example_protos: tf.Tensor):
 
     white = tf.io.decode_raw(parsed["white"], tf.uint16)
     black = tf.io.decode_raw(parsed["black"], tf.uint16)
-    material_value = tf.io.decode_raw(parsed["material_value"], tf.uint8)
+    material_value = tf.io.decode_raw(parsed["material_value"], tf.uint16)
     turn = tf.io.decode_raw(parsed["turn"], tf.uint8)
     score = tf.io.decode_raw(parsed["score"], tf.float32)
 